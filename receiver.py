ef9ec99801ba43d1b93b79ace1c83a631ee6d304
Stsiapan <Kinkard@users.noreply.github.com>
2019-07-12 22:48:47 +0300

    added draft file handling logic

--- receiver.py
+++ receiver.py
@@ -2,15 +2,17 @@ import paho.mqtt.client as mqtt
 
 client = mqtt.Client("box")
 
+file_name = None
 def process_video_upload(client, userdata, message):
-    print("Hue!")
-    #print("message received " ,str(message.payload.decode("utf-8")))
-    print("message topic=",message.topic)
-    #print("message qos=",message.qos)
+    if file_name is None:
+        file_name = str(message.payload.decode("utf-8"))
+    elif len(message.payload) == 0:
+        print("Received file " + file_name + " from " + str(client))
+        file_name = None
 
 # register all callbacks during initialisation
 client.message_callback_add("device/video", process_video_upload)
 
 client.connect("192.168.1.23")
-client.subscribe("device/#")
+client.subscribe("device/#", 2)
 client.loop_forever()
