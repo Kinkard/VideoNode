6a86fc70ddc33e07d32c25b29e22b93cb28ef7bf
Stsiapan <Kinkard@users.noreply.github.com>
2019-07-12 21:56:22 +0300

    added counter for messages

--- receiver.py
+++ receiver.py
@@ -3,10 +3,13 @@ import paho.mqtt.client as mqtt
 client = mqtt.Client("box")
 client.connect("192.168.1.23")
 
+counter = 0 # input messages counter
 def on_message(client, userdata, message):
-    print("message received " ,str(message.payload.decode("utf-8")))
-    print("message topic=",message.topic)
-    print("message qos=",message.qos)
+    counter += 1
+    print(counter)
+    #print("message received " ,str(message.payload.decode("utf-8")))
+    #print("message topic=",message.topic)
+    #print("message qos=",message.qos)
 
 client.subscribe("device/video")
 client.on_message=on_message
