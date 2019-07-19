241fea387f7d2bfd040c1387d9d288c5a680cb8c
Kinkard <stsiapan.kizim@gmail.com>
2019-07-19 23:07:51 +0300

    added file download via SFTP from host and MediaInfo display

--- receiver.py
+++ receiver.py
@@ -1,16 +1,22 @@
+import datetime
 import paho.mqtt.client as mqtt
 import paramiko
+from pymediainfo import MediaInfo
 
-host = "192.168.1.23"
-transport = paramiko.Transport((host, 22))
-transport.connect(username = "pi", password = "cntgfyrbpbv")
-sftp = paramiko.SFTPClient.from_transport(transport)
+host = '192.168.1.23'
+ssh = paramiko.SSHClient()
+ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+ssh.connect(host, username='pi', password = 'cntgfyrbpbv')
+sftp = ssh.open_sftp()
 
 def process_video(client, userdata, message):
     file_name = str(message.payload, 'utf-8')
-    print("Requested video " + file_name)
-    sftp.get("/" + file_name, file_name)
-    print(file_name + " downloaded!")
+    print("Requested video {0}".format(file_name))
+    sftp.get("/" + file_name, file_name, callback=lambda d, t : print('Transferred: {0:3.2f}%'.format(100 * (d/t)), end='\r'))
+
+    # reading video duration
+    info = MediaInfo.parse(file_name)
+    print("Downloaded {0} with duration {1}".format(file_name, datetime.timedelta(milliseconds=info.tracks[0].duration)))
 
 # set up MQTT communication
 client = mqtt.Client("box")
